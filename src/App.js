2ba80a71f6f8cc17ed2f9e9e5da8919ed366f91e
Jon Courtier <jonaqwerty@yahoo.com>
2022-05-15 05:02:17 -0400

    adjusting sass foundation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header"
 import FaceContainer from "./FaceContainer"
 
@@ -11,11 +12,42 @@ useEffect(() => {
   .then((emoData) => setEmotions(emoData));
 }, [])
 
+// const cards = [
+//   {
+//     id: "1",
+//     variant: "hover",
+//     front: "Hover",
+//     back: "Back"
+//   },
+//   {
+//     id: "2",
+//     variant: "click",
+//     front: "Click",
+//     back: "Back"
+//   },
+//   {
+//     id: "3",
+//     variant: "focus",
+//     front: "Focus",
+//     back: "Back"
+//   }
+// ];
+
   return (
     <div className="App">
       <Header />
       <FaceContainer emotions={emotions}/>
+      {/* <div className="container">
+      <div className="row h-100">
+        <div class="col d-flex flex-column flex-md-row justify-content-around align-items-center">
+          {cards.map((card) => (
+            <FlipCard key={card.id} card={card} />
+          ))}
+        </div>
+      </div>
+    </div> */}
     </div>
+    
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div>
             <h2>SpectrumSpecs</h2>
-            <h3>intro and Autism definition</h3>
+            <h3>Intro and Autism Definition</h3>
             <p>Intro to site need and facial expression</p>
         </div>
     )
